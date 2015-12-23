4e41a26035ba353f8602e97048d86edf1b279f52
Justin Moh <j.mk.moh@gmail.com>
2015-12-24 00:25:12 +0800

    Corrected path to .bash_profile and php-fpm.conf

--- scripts/clear-variables.sh
+++ scripts/clear-variables.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Clear The Old Environment Variables
 
-sed -i '/# Set Mockingj Environment Variable/,+1d' /home/vagrant/.profile
-sed -i '/env\[.*/,+1d' /etc/php-fpm/php-fpm.conf
+sed -i '/# Set Mockingj Environment Variable/,+1d' /home/vagrant/.bash_profile
+sed -i '/env\[.*/,+1d' /etc/php-fpm.conf
